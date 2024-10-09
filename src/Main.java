6f1382719ade9f41a96d00bc037841ed76e96148
ПК <bic3337@gmail.com>
2024-10-09 23:54:02 +0300

    Выполнение Д/З.

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,43 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work 3!");
         int age = 15;
-        System.out.print("Если возрот человека равен "+age+" то,");
+        System.out.print(" Возрот человека равен "+age+" то,");
         if ( age >= 18) {
             System.out.println(" Вы  свершеннолетний !! ");
         }
         if (age<18) {
             System.out.println(" Вам еще нет совершеннолетия, нужно немног подождать !!");
         }
-int temperatura = -13;
+        int temperatura = -13;
         System.out.print("На улице "+ temperatura+ " градусов,");
         if (temperatura <=5){
             System.out.println(" нужно одеть шапку ");
         } else {
             System.out.println(" можно идти без шапки. ");
         }
+        int speedOfTheCar = 100 ;
+        System.out.print(" Скорость машины "+ speedOfTheCar+ " км/ч");
+        if (speedOfTheCar > 60) {
+            System.out.println(" прийдется оплатить штраф !!!!!");
 
+        } else {
+            System.out.println(" можно ездить спокойно. ");
+        }
 
+        int ageOfTheperson= 14;
+        System.out.print("Если возрост человека равен  "+ageOfTheperson);
+        if (ageOfTheperson>2 && ageOfTheperson<6 ){
+            System.out.println(" то ему нужно ходить в садик .");
+        }
+        if (ageOfTheperson>7 && ageOfTheperson<17 ){
+            System.out.println(" то ему нужно ходить в школу.");
+        }
+        if(ageOfTheperson>=18 && ageOfTheperson<=24){
+            System.out.println(" то ему место  в университете.");
+        }
+        if(ageOfTheperson>24){
+            System.out.println(" то ему пора ходить на работу.");
+        }
 
 
         }
