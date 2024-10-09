0b18e0570e42fa83eced959ae777c24569700754
ПК <bic3337@gmail.com>
2024-10-09 22:36:30 +0300

    Выполнение Д/З.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work 3!");
+        int age = 15;
+        System.out.print("Если возрот человека равен "+age+" то,");
+        if ( age >= 18) {
+            System.out.print(" Вы  свершеннолетний !! ");
+        }
+        if (age<18) {
+            System.out.print(" Вам еще нет совершеннолетия, нужно немног подождать !!");
+        }
+
+
+
+        }
     }
-}
